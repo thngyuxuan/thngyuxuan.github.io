6cf49b8753079622850bb39fdf127d9aa6bf5707
Thng Yu Xuan <thngyuxuan@gmail.com>
2022-06-28 10:13:29 +0800

    responsive webpage

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -41,14 +41,14 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
+"flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e3b5814b0dd03bae7170fbd6d7709823",
-"/": "e3b5814b0dd03bae7170fbd6d7709823",
-"main.dart.js": "d7e5dce2be3d06666115b30a1cd78825",
+"index.html": "ab3ce6bb8408e27d33cedb9f45219e6a",
+"/": "ab3ce6bb8408e27d33cedb9f45219e6a",
+"main.dart.js": "290f887734b2f0174c3d1f0e14d0fd46",
 "manifest.json": "660a8775587c6a651c777ead70ff542b",
 "version.json": "6782f104daa381bde1e9c635c50fed35"
 };
